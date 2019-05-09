6f318a0f53f22994d59c4942d4d0b664ef918839
if1live <libsora25@gmail.com>
2019-05-09 12:01:54 +0900

    폰트 교체

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const docsPath = path.resolve(__dirname, '../docs');
 
 app.use('/static', express.static(path.resolve(docsPath, 'static')));
 app.use('/assets', express.static(path.resolve(docsPath, 'assets')));
+app.use('/fonts', express.static(path.resolve(docsPath, 'fonts')));
 
 app.get('/', (req, res) => {
 	// TODO 템플릿으로 갈아끼우기
