09f825974a631de41406008b6fed777fcfd3643f
if1live <libsora25@gmail.com>
2019-05-09 12:23:15 +0900

    페이지 빌드

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -o docs/index.html http://127.0.0.1:3000/
+curl -o docs/index-en.html http://127.0.0.1:3000/index-en.html
+curl -o docs/index-ko.html http://127.0.0.1:3000/index-ko.html
