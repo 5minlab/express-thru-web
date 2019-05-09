749e438ab0dbcf82e0d9ea3d66fa282b99ce4ee7
if1live <libsora25@gmail.com>
2019-05-09 10:45:22 +0900

    썸네일 생성. 스크린샷 tinypng로 용량 줄임

---
+++ docs/static/img/screenshot/resize.bat
@@ -0,0 +1,6 @@
+@rem make thumbnail
+magick convert s01.png -resize "25%%" thumbnail-1.jpg
+magick convert s02.png -resize "25%%" thumbnail-2.jpg
+magick convert s03.png -resize "25%%" thumbnail-3.jpg
+magick convert s04.png -resize "25%%" thumbnail-4.jpg
+magick convert s05.png -resize "25%%" thumbnail-5.jpg
